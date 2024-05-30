c8bc97d656028a2f33ab84c953604643c1f19c19
lleomllr <88625636+lleomllr@users.noreply.github.com>
2024-05-30 20:09:55 +0200

    Add files via upload

---
+++ DeepArt/setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 30 20:01:17 2024
+
+@author: meill
+"""
+from setuptools import setup, find_packages
+
+
+VERSION = '1.0.0'
+
+setup(
+      name='deepart', 
+      version=VERSION, 
+      description='Deep Art', 
+      url='https://github.com/lleomllr/DeepArt', 
+      keywords=['image processing', 'art', 'deepart', 'computer vision', 'image recognition'], 
+      author='Leo Meillier', 
+      author_email='meillier.leo@gmail.com', 
+)
