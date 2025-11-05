13f6b6e3d1cb4958e310df0b03376a4a53ea7f98
LeoM <meillier.leo@gmail.com>
2025-11-05 15:00:37 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0.0'
+
+setup(
+      name='deepart', 
+      version=VERSION, 
+      description='Deep Art', 
+      url='https://github.com/lleomllr/DeepArt', 
+      keywords=['image processing', 'art', 'deepart', 'computer vision', 'image recognition'], 
+      author='Leo Meillier', 
+      author_email='meillier.leo@gmail.com', 
+)
